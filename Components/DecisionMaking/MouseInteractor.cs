84bd66073ed6e8153e3e5d79cd4a6b629024f1c0
John Harger <john.harger@gmail.com>
2010-08-09 00:01:31 -0600

    Fixed a bug relating to moving without a target. Now checks for multiple raycast hits and picks the first hit UnityNode. Configurable movement mouse button.

--- Components/DecisionMaking/MouseInteractor.cs
+++ Components/DecisionMaking/MouseInteractor.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 [AddComponentMenu("Sanity Engine/User Interaction/Mouse Interactor")]
 public class MouseInteractor : DecisionMaker {
+	public enum MouseButton {
+		LEFT = 0,
+		RIGHT = 1,
+		MIDDLE = 2
+	}
+	
 	public LayerMask clickLayerMask = -1;
+	public MouseButton moveButton = MouseButton.RIGHT;
 	UnityNode moveTarget;
 	
 	void OnGUI()
@@ -12,18 +19,20 @@ public class MouseInteractor : DecisionMaker {
 			return;
 		}
 		
-		if(Event.current.button != 0) {
+		if(Event.current.button != (int)moveButton) {
 			return;
 		}
 		
 		Vector3 pos = Event.current.mousePosition;
 		pos.y = Screen.height - pos.y;
 		Ray ray = Camera.main.ScreenPointToRay(pos);
-		RaycastHit hit;
-		if(Physics.Raycast(ray, out hit, Mathf.Infinity, -1))
-		{
-			// TODO iterate through all, searching for a UnityNode
-			moveTarget = hit.transform.GetComponent<UnityNode>();
+		RaycastHit[] hits = Physics.RaycastAll(ray, Mathf.Infinity,
+			clickLayerMask.value);
+		foreach(RaycastHit hit in hits)	{
+			UnityNode node = hit.transform.GetComponent<UnityNode>();
+			if(node != null) {
+				moveTarget = node;
+			}
 		}
 		
 		Event.current.Use();
