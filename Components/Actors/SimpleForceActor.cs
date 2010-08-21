655b44bdaa4e1479568f1fa692eda5c5aa92cc37
John Harger <john.harger@gmail.com>
2010-08-21 17:04:21 -0600

    Fixed the rotation of the actors

--- Components/Actors/SimpleForceActor.cs
+++ Components/Actors/SimpleForceActor.cs
@@ -5,10 +5,13 @@ using SanityEngine.Movement.SteeringBehaviors;
 [AddComponentMenu("Sanity Engine/Actors/Simple Force Actor")]
 [RequireComponent(typeof(SteeringManagerComponent))]
 public class SimpleForceActor : GameObjectActor {
+	public float mass = 1.0f;
 	public float maxForce = 2.5f;
 	public float maxSpeed = 5.0f;
-	public float maxAngularForce = 2.5f;
+	public float maxTorque = 2.5f;
 	public float maxAngularSpeed = 5.0f;
+	public float angleThreshold = 90.0f;
+	public float velocityFaceThreshold = 0.001f;
 	public float linearDamp = 0.05f;
 	public float angularDamp = 0.05f;
 	public bool twoDimensionalFacing = true;
@@ -28,39 +31,50 @@ public class SimpleForceActor : GameObjectActor {
 	
 	void LateUpdate ()
 	{
-		Vector3 force = manager.Force;
-		velocity += force;
-		velocity *= 1f - linearDamp;
+		float t = Time.deltaTime;
+		Vector3 desired = (manager.Force - velocity) / t;
+		float force = desired.magnitude;
+		if(force > maxForce) {
+			desired *= maxForce / force;
+		}
+		Vector3 accel = desired / mass;
+		xform.Translate(velocity * t + 0.5f * accel * t * t, Space.World);
+		velocity += accel * t;
+		velocity -= velocity * linearDamp;
 		float speed = velocity.magnitude;
 		if(speed > maxSpeed) {
 			velocity *= maxSpeed / speed;
 		}
-		xform.position += velocity;
 		
 		Vector3 dir = xform.forward;
-		if(velocity.magnitude > 0.001f) {
+		if(velocity.magnitude > velocityFaceThreshold) {
 			dir = velocity / velocity.magnitude;
 		}
 		Vector3 delta = Quaternion.FromToRotation(xform.forward, dir).eulerAngles;
 		delta.x = twoDimensionalFacing ? 0f : Mathf.DeltaAngle(0f, delta.x);
 		delta.y = Mathf.DeltaAngle(0f, delta.y);
 		delta.z = 0.0f;
-			
-		delta /= 5.0f;
-		float a = delta.magnitude;
-		if(a > maxAngularForce) {
-			delta *= maxAngularForce / a;
+		float angDist = delta.magnitude;
+		if(angDist > 0.0f) {
+			float torque = maxTorque * (angDist / angleThreshold);
+			torque = Mathf.Min(torque, maxTorque);
+			delta *= torque / angDist;
 		}
 		delta -= angularVelocity;
-		angularVelocity += delta;
 		
-		angularVelocity *= 1f - angularDamp;
+		Vector3 angAccel  = (delta - angularVelocity) / t;
+		float torque2 = angAccel.magnitude;
+		if(torque2 > maxTorque) {
+			angAccel *= maxTorque / torque2;
+		}
+		
+		xform.Rotate(angularVelocity * t + angAccel * t * t, Space.World);
+		angularVelocity += angAccel * t;
+		angularVelocity -= angularVelocity * angularDamp;
 		float angSpeed = angularVelocity.magnitude;
 		if(angSpeed > maxAngularSpeed) {
 			angularVelocity *= maxAngularSpeed / angSpeed;
 		}
-		xform.Rotate(angularVelocity, Space.World);
-		
 	}
 		
 	public override Vector3 Velocity
@@ -80,6 +94,6 @@ public class SimpleForceActor : GameObjectActor {
 
 	public override float MaxForce
 	{
-        get { return maxForce; }
+        get { return maxSpeed; }
     }
 }
